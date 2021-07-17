00829ead623df626e363d04f11a08b631f5c076f
DiogoValente22 <diogovalentearagao@outlook.com>
2021-07-16 21:01:05 -0300

    finished search bar toggle script

--- assets/js/users.js
+++ assets/js/users.js
@@ -1,1 +1,10 @@
-const searchBar = document.query
+const searchBar = document.querySelector(".users .search input");
+searchBtn = document.querySelector(".users .search button");
+
+function searchToggle(){
+    searchBar.classList.toggle("active");
+    searchBar.focus();
+    searchBtn.classList.toggle("active");
+}
+
+searchBtn.addEventListener("click", searchToggle);
