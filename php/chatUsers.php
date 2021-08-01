c3e10773831d9f574f6e992e54744b226ad67da4
DiogoValente22 <diogovalentearagao@outlook.com>
2021-08-01 15:32:03 -0300

    feat: show all users

---
+++ php/chatUsers.php
@@ -0,0 +1,27 @@
+<?php
+
+    session_start();
+    include_once "config.php";
+
+    //count rows query
+    $stmt_count = $conn->prepare("SELECT count(*) FROM users");
+    $stmt_count->execute();
+    $result_count = $stmt_count->fetchAll(PDO::FETCH_ASSOC);
+
+    $rows_number = $result_count[0]['count(*)'];
+
+    //query to select all data from users
+    $stmt = $conn->prepare("SELECT * FROM users");
+    $stmt->execute();
+    
+    $output = "";
+
+    if($rows_number == 1){
+        $output = "No users are available to chat";
+    }elseif($rows_number > 0){
+        include "data.php";
+    }
+
+    echo $output;
+
+?>
