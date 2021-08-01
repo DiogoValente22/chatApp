a97d34e4f5455c16e6ef161e216f74a0bb8e1a05
DiogoValente22 <diogovalentearagao@outlook.com>
2021-08-01 15:35:08 -0300

    feat: search user

--- assets/js/users.js
+++ assets/js/users.js
@@ -1,10 +1,55 @@
-const searchBar = document.querySelector(".users .search input");
-searchBtn = document.querySelector(".users .search button");
+const searchBar = document.querySelector(".users .search input"),
+searchBtn = document.querySelector(".users .search button"),
+usersList = document.querySelector(".users .users-list");
 
 function searchToggle(){
     searchBar.classList.toggle("active");
     searchBar.focus();
     searchBtn.classList.toggle("active");
+    searchBar.value = "";
+}
+searchBtn.addEventListener("click", searchToggle);
+
+//Search User AJAX
+searchBar.onkeyup = ()=>{
+    let searchTerm = searchBar.value;
+
+    if(searchTerm != ""){
+        searchBar.classList.add("active");
+    }else{
+        searchBar.classList.remove("active");
+    }
+    
+    // starting AJAX
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/user_search.php");
+    xhr.onload = ()=>{
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status === 200){
+                let data = xhr.response;
+                usersList.innerHTML = data;
+            }
+        }
+    }
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("searchTerm=" + searchTerm);
+
 }
 
-searchBtn.addEventListener("click", searchToggle);
+// Show userList AJAX
+setInterval(()=>{
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "php/chatUsers.php");
+    xhr.onload = ()=>{
+        if(xhr.readyState == XMLHttpRequest.DONE){
+            if(xhr.status == 200){
+                let data = xhr.response;
+                if(!searchBar.classList.contains("active")){
+                    usersList.innerHTML = data;
+                }
+                
+            }
+        }
+    }
+    xhr.send();
+}, 500);
