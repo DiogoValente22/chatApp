337066e803139ee447d86f815413302293bb86fb
DiogoValente22 <diogovalentearagao@outlook.com>
2021-08-04 14:06:41 -0300

    feat: added auto scroll in chat

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -7,6 +7,10 @@ form.onsubmit = (event)=>{
     event.preventDefault();
 }
 
+function scrollToBottom(){
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
+
 sendBtn.onclick = ()=>{
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "php/insert-chat.php", true);
@@ -14,11 +18,19 @@ sendBtn.onclick = ()=>{
         if(xhr.readyState === XMLHttpRequest.DONE){
             if(xhr.status === 200){
                 inputField.value = "";
+                scrollToBottom();
             }
         }
     }
     let formData = new FormData(form);
     xhr.send(formData);
+}
+
+chatBox.onmouseenter = ()=>{
+    chatBox.classList.add("active");
+}
+chatBox.onmouseleave = ()=>{
+    chatBox.classList.remove("active");
 }
 
 setInterval(()=>{
@@ -29,9 +41,13 @@ setInterval(()=>{
             if(xhr.status == 200){
                 let data = xhr.response;
                 chatBox.innerHTML = data;
+                if(!chatBox.classList.contains("active")){
+                    scrollToBottom();
+                }
             }
         }
     }
     let formData = new FormData(form);
     xhr.send(formData);
 }, 500);
+
